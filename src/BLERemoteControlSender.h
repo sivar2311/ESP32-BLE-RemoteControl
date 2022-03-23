40ba94800ce0758552e01e4ca67a08c14876f86a
sivar2311 <sivar2311@googlemail.com>
2022-03-23 06:49:56 +0100

    Added - storage support - synchronizing / adding remote via secure ble connection

--- src/BLERemoteControlSender.h
+++ src/BLERemoteControlSender.h
@@ -6,22 +6,59 @@
 
 class BLERemoteControlSender : public BLERemoteControlBaseClass {
   public:
-    void begin();
+    void begin(uint32_t pin = 123456);
 
     void send_command(uint32_t command, ble_remote_control_info_t* remote_control);
+    void start_sync(ble_remote_control_info_t& remote_control);
 
   protected:
     NimBLEAdvertising* ble_advertising = nullptr;
+
+    NimBLEAdvertisementData sync_adv_data;
+
+    NimBLEServer*         ble_server         = nullptr;
+    NimBLEService*        ble_service        = nullptr;
+    NimBLECharacteristic* ble_characteristic = nullptr;
 };
 
-void BLERemoteControlSender::begin() {
+void BLERemoteControlSender::begin(uint32_t pin) {
     NimBLEDevice::init("");
+
+    NimBLEDevice::setPower(ESP_PWR_LVL_P9);
+    NimBLEDevice::setSecurityAuth(true, true, true);
+    NimBLEDevice::setSecurityPasskey(pin);
+    NimBLEDevice::setSecurityIOCap(BLE_HS_IO_DISPLAY_ONLY);
+
+    ble_server = NimBLEDevice::createServer();
+    ble_server->advertiseOnDisconnect(false);
+
+    ble_service        = ble_server->createService(NimBLEUUID(remote_control_service_uuid));
+    ble_characteristic = ble_service->createCharacteristic(
+        NimBLEUUID(remote_control_characteristic_uuid),
+        NIMBLE_PROPERTY::READ |
+        NIMBLE_PROPERTY::READ_ENC |
+        NIMBLE_PROPERTY::READ_AUTHEN 
+    );
+    ble_service->start();
+
     ble_advertising = NimBLEDevice::getAdvertising();
+    ble_advertising->setMinInterval(0x20);
+    ble_advertising->setMaxInterval(0xA0);
+
+    sync_adv_data.setFlags(0x06);
+    sync_adv_data.setCompleteServices(NimBLEUUID(remote_control_service_uuid));
+}
+
+void BLERemoteControlSender::start_sync(ble_remote_control_info_t& remote_control) {
+    ble_characteristic->setValue(remote_control);
+    remote_control.rolling_code++;
+
+    ble_advertising->setAdvertisementType(BLE_GAP_CONN_MODE_UND);
+    ble_advertising->setAdvertisementData(sync_adv_data);
+    ble_advertising->start(5);
 }
 
 void BLERemoteControlSender::send_command(uint32_t command, ble_remote_control_info_t* remote_control) {
-    if (ble_advertising == nullptr) return;
-
     BLEManufacturerData man_data;
     man_data.company_id = 0xFFFF;
     man_data.remote_id  = remote_control->id;
@@ -34,8 +71,13 @@ void BLERemoteControlSender::send_command(uint32_t command, ble_remote_control_i
 
     encrypt_message(man_data.encrypted_message, &message, remote_control->aes_key);
 
-    ble_advertising->stop();
-    ble_advertising->setManufacturerData(std::string((const char*)&man_data, sizeof(BLEManufacturerData)));
+    NimBLEAdvertisementData adv_data;
+ 
+    adv_data.setFlags(0x00);
+    adv_data.setManufacturerData(std::string((const char*)&man_data, sizeof(BLEManufacturerData)));
+ 
     ble_advertising->setAdvertisementType(BLE_GAP_CONN_MODE_NON);
+    ble_advertising->setAdvertisementData(adv_data);
+
     ble_advertising->start(1);
 }
