8d51c45f8fabe3f4811c50f85f977535707e1296
sivar2311 <sivar2311@googlemail.com>
2022-03-23 07:07:32 +0100

    fixed examples to match the new version

--- examples/Sender/Sender.ino
+++ examples/Sender/Sender.ino
@@ -1,34 +1,45 @@
 #include <Arduino.h>
 
-#include "BLERemoteControlSender.h"
+#include <BLERemoteControlSender.h>
+#include <NVSRemoteControlStorage.h>
 
+NVSRemoteControlStorage storage;
 BLERemoteControlSender sender;
 
-#define GPIO_BUTTON_1 34
-#define GPIO_BUTTON_2 35
+#define GPIO_BUTTON_ON 34
+#define GPIO_BUTTON_OFF 35
+#define GPIO_BUTTON_SYNC 36
 
 #define PRESSED HIGH
-
-#define REMOTE_ID 0x0001
 
 #define COMMAND_LED_ON  0x0001
 #define COMMAND_LED_OFF 0x0002
 
-ble_remote_control_info_t remote_control(REMOTE_ID, {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F});
+
+void setup_storage() {
+    storage.begin("ble_remote");
+    if (!storage.load()) {
+        storage.add(new ble_remote_control_info_t(0x0001,true));
+        storage.save();
+    }
+}
 
 void setup_buttons() {
-    pinMode(GPIO_BUTTON_1, INPUT);
-    pinMode(GPIO_BUTTON_2, INPUT);
+    pinMode(GPIO_BUTTON_ON, INPUT);
+    pinMode(GPIO_BUTTON_OFF, INPUT);
+    pinMode(GPIO_BUTTON_SYNC, INPUT);
 }
 
 void handle_buttons() {
-    bool button_1 = digitalRead(GPIO_BUTTON_1);
-    bool button_2 = digitalRead(GPIO_BUTTON_2);
+    bool button_on = digitalRead(GPIO_BUTTON_ON);
+    bool button_off = digitalRead(GPIO_BUTTON_OFF);
+    bool button_sync = digitalRead(GPIO_BUTTON_SYNC);
 
-    if (button_1 == PRESSED) sender.send_command(COMMAND_LED_ON, &remote_control);
-    if (button_2 == PRESSED) sender.send_command(COMMAND_LED_OFF, &remote_control);
+    if (button_on == PRESSED) sender.send_command(COMMAND_LED_ON, storage[0]);
+    if (button_off == PRESSED) sender.send_command(COMMAND_LED_OFF, storage[0]);
+    if (button_sync == PRESSED) sender.start_sync(storage[0]);
 
-    if (button_1 == PRESSED || button_2 == PRESSED) {
+    if (button_on == PRESSED || button_off == PRESSED || button_sync == PRESSED) {
         delay(150); // simple "debounce"
     }
 }
